96d6c0d8961f2fc2d607434249b5637a6c209dbd
Hammad Daoud <hdaoud@nisum.com>
2022-03-16 10:13:05 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,2 @@
+kubectl apply -f /var/lib/jenkins/workspace/3-tier-pipeline/kubemanifests.yaml
 kubectl patch service gateway-tcp -n default -p '{"spec": {"type": "LoadBalancer", "externalIPs":["137.184.226.30"]}}'
